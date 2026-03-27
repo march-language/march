76126effbfbefe689a49520b02b5022e2438fdfd
Matthew Gilliam <chase.gilliam@gmail.com>
2026-03-26 22:44:10 -0400

    fix(zed): use Zed's injection capture names (@content, @language)
    Zed uses non-standard tree-sitter capture names: @content instead of
    @injection.content, and @language instead of @injection.language.
    The standard names broke all syntax highlighting.

--- zed-march/languages/march/injections.scm
+++ zed-march/languages/march/injections.scm
@@ -1,12 +1,12 @@
 ; Inject HTML highlighting into ~H sigil content
 ((sigil_expression
-  prefix: (sigil_prefix) @_prefix
-  content: (string) @injection.content)
- (#eq? @_prefix "~H")
- (#set! injection.language "html"))
+  prefix: (sigil_prefix) @language
+  content: (string) @content)
+ (#eq? @language "~H")
+ (#set! language "html"))
 
 ((sigil_expression
-  prefix: (sigil_prefix) @_prefix
-  content: (triple_string) @injection.content)
- (#eq? @_prefix "~H")
- (#set! injection.language "html"))
+  prefix: (sigil_prefix) @language
+  content: (triple_string) @content)
+ (#eq? @language "~H")
+ (#set! language "html"))
