f999b270cd927158af2725fee8e6478cf9ab1e00
Matthew Gilliam <chase.gilliam@gmail.com>
2026-06-21 19:39:03 -0400

    fix(tir): set ed_raises on nested-module extern decls (merge artifact)

--- lib/tir/lower.ml
+++ lib/tir/lower.ml
@@ -2168,6 +2168,7 @@ let lower_module ?type_map ?(stdlib_context : Ast.decl list = []) ?(test_mode=fa
                                  ed_params = params;
                                  ed_consumed = ef.ef_param_consumed;
                                  ed_blocking = ef.ef_blocking;
+                                 ed_raises = ef.ef_raises;
                                  ed_ret = ret } :: !externs
                   ) edef.ext_fns
               | _ -> ()
